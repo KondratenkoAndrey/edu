547bd5571bdeb1f3814bcd0ca7362a2a602ed261
Andrey <kondratenkoav@outlook.com>
2018-05-18 20:30:32 +0300

    Added homeWorkBuildMapValuesSet

---
+++ week 2/homeWorkBuildMapValuesSet/homeWorkBuildMapValuesSet/function.hpp
@@ -0,0 +1,22 @@
+//
+//  function.hpp
+//  homeWorkBuildMapValuesSet
+//
+//  Created by Андрей Кондратенко on 18.05.2018.
+//  Copyright © 2018 Andrey Kondratenko. All rights reserved.
+//
+
+#ifndef function_hpp
+#define function_hpp
+
+#include <stdio.h>
+#include <set>
+#include <map>
+#include <string>
+
+using namespace std;
+
+#endif /* function_hpp */
+
+
+set<string> BuildMapValuesSet(const map<int, string>& dict);
