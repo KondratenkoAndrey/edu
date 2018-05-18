977c61f42478ac61b45a5d26a3f77f069f427bf3
Andrey <kondratenkoav@outlook.com>
2018-05-18 13:09:11 +0300

    Added homeWorkBusStations part 2

---
+++ week 2/homeWorkBusStation2/homeWorkBusStation2/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  homeWorkBusStation2
+//
+//  Created by Андрей Кондратенко on 18.05.2018.
+//  Copyright © 2018 Andrey Kondratenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    return 0;
+}
