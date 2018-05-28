3fdf47ec35842875d91882af094206b5ca2b0099
Andrey <kondratenkoav@outlook.com>
2018-05-28 21:30:51 +0300

    Added homeWorkInitializationMethods

---
+++ week 3/homeWorkInitializationMethods/homeWorkInitializationMethods/Incognizable.cpp
@@ -0,0 +1,18 @@
+//
+//  Incognizable.cpp
+//  homeWorkInitializationMethods
+//
+//  Created by Андрей Кондратенко on 28.05.2018.
+//  Copyright © 2018 Andrey Kondratenko. All rights reserved.
+//
+
+#include <stdio.h>
+
+using namespace std;
+
+struct Incognizable {
+
+    int someParameter = 0;
+    int anotherParameter = 0;
+    
+};
---
+++ week 3/homeWorkInitializationMethods/homeWorkInitializationMethods/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  homeWorkInitializationMethods
+//
+//  Created by Андрей Кондратенко on 28.05.2018.
+//  Copyright © 2018 Andrey Kondratenko. All rights reserved.
+//
+
+#include <iostream>
+#include "Incognizable.cpp"
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+
+    Incognizable a;
+    Incognizable b = {};
+    Incognizable c = {0};
+    Incognizable d = {0, 1};
+    
+    return 0;
+}
