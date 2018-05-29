cb85e1b404d935122fac7f5e503bff9c13294d04
Andrey <kondratenkoav@outlook.com>
2018-05-29 23:36:05 +0300

    Added formattedStream

---
+++ week 4/formattedStream/formattedStream/main.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  formattedStream
+//
+//  Created by Андрей Кондратенко on 29.05.2018.
+//  Copyright © 2018 Andrey Kondratenko. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+void Print(const vector<string>& names,
+           const vector<double>& values,
+           int width) {
+    for (const string& name : names) {
+        cout << setw(width) << name << " ";
+    }
+    cout << endl;
+    cout << fixed << setprecision(2);
+    for (const double& value : values) {
+        cout << setw(width) << value << " ";
+    }
+    cout << endl;
+}
+
+int main(int argc, const char * argv[]) {
+
+    vector<string> names = {"a", "b", "c"};
+    vector<double> values = {5, 0.01, 0.000005};
+    
+    cout << setfill('.');
+    cout << left;
+    Print(names, values, 10);
+    
+    return 0;
+}
