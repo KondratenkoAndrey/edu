fca2fcd6ac26ff791c4319a472b994c5a7f76442
Andrey <kondratenkoav@outlook.com>
2018-06-05 23:22:37 +0300

    Added homeWorkClassRationalNumbers

--- week 4/homeWorkClassRationalNumbers/homeWorkClassRationalNumbers/rational_map.cpp
+++ week 4/homeWorkClassRationalNumbers/homeWorkClassRationalNumbers/rational_map.cpp
@@ -2,13 +2,106 @@
 #include <map>
 #include <set>
 #include <vector>
+#include <sstream>
+
 using namespace std;
 
+int calculateNod(int n, int d){
+    n = abs(n);
+    d = abs(d);
+    int nod = max(n, d);
+    for (int i = min(n, d); i > 0; i--) {
+        if (n % i == 0 && d % i == 0) {
+            nod = i;
+            break;
+        }
+    }
+    return nod;
+}
+
 class Rational {
-    // Вставьте сюда реализацию класса Rational из первой части
+public:
+    Rational() {
+        //конструктор по умолчанию
+        rNumber.numerator = 0;
+        rNumber.denominator = 1;
+    }
+    
+    Rational(int numerator, int denominator) {
+        //конструктор
+        int nod = calculateNod(numerator, denominator);
+        if ((numerator >= 0 && denominator > 0) || (numerator < 0 && denominator < 0)){
+            rNumber.numerator = abs(numerator)/nod;
+        } else {
+            rNumber.numerator = -1 * abs(numerator)/nod;
+        }
+        rNumber.denominator = abs(denominator)/nod;
+    }
+    
+    int Numerator() const {
+        return rNumber.numerator;
+    }
+    
+    int Denominator() const {
+        return rNumber.denominator;
+    }
+    
+private:
+    struct RationalNumber {
+        int numerator;
+        int denominator;
+    };
+    RationalNumber rNumber;
 };
 
-// Вставьте сюда реализацию operator == для класса Rational из второй части
+bool operator==(const Rational& lhs, const Rational& rhs) {
+    return (lhs.Numerator() == rhs.Numerator()) && (lhs.Denominator() == rhs.Denominator());
+}
+
+Rational operator+(const Rational& lhs, const Rational& rhs) {
+    return Rational(lhs.Numerator() * rhs.Denominator() + lhs.Denominator() * rhs.Numerator(),
+                    lhs.Denominator() * rhs.Denominator());
+}
+
+Rational operator-(const Rational& lhs, const Rational& rhs) {
+    return Rational(lhs.Numerator() * rhs.Denominator() - lhs.Denominator() * rhs.Numerator(),
+                    lhs.Denominator() * rhs.Denominator());
+}
+
+Rational operator*(const Rational& lhs, const Rational& rhs) {
+    return Rational(lhs.Numerator() * rhs.Numerator(),
+                    lhs.Denominator() * rhs.Denominator());
+}
+
+Rational operator/(const Rational& lhs, const Rational& rhs) {
+    return Rational(lhs.Numerator() * rhs.Denominator(),
+                    lhs.Denominator() * rhs.Numerator());
+}
+
+ostream& operator<<(ostream& stream, const Rational& r) {
+    stream << r.Numerator() << "/" << r.Denominator();
+    return stream;
+}
+
+istream& operator>>(istream& stream, Rational& r) {
+    
+    string str;
+    stream >> str;
+    istringstream newStream(str); //перевод через промежуточную строку для исключени ошибки с пробелом
+    
+    if (!str.empty()) { //проверка на пустой поток
+        int n, d;
+        newStream >> n;
+        newStream.ignore(1);
+        newStream >> d;
+        r = {n, d};
+    }
+    return stream;
+}
+
+bool operator<(const Rational& lhs, const Rational& rhs) {
+    return lhs.Numerator() * rhs.Denominator() < rhs.Numerator() * lhs.Denominator();
+}
 
 // Реализуйте для класса Rational оператор(ы), необходимые для использования его
 // в качестве ключа map'а и элемента set'а
