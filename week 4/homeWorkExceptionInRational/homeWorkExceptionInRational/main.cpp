0a3e3fc31d3cabc7453c5d5e8df6d3001128cd2e
Andrey <kondratenkoav@outlook.com>
2018-06-06 23:04:15 +0300

    Added homeWorkExceptionInRational

---
+++ week 4/homeWorkExceptionInRational/homeWorkExceptionInRational/main.cpp
@@ -0,0 +1,18 @@
+////
+////  main.cpp
+////  homeWorkExceptionInRational
+////
+////  Created by Андрей Кондратенко on 06.06.2018.
+////  Copyright © 2018 Andrey Kondratenko. All rights reserved.
+////
+//
+//#include <iostream>
+//
+//
+//
+//int main(int argc, const char * argv[]) {
+//
+//    
+//    
+//    return 0;
+//}
