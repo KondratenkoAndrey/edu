2aab0f1fc432c462c8bc5e98ae1542faeec62979
Andrey <kondratenkoav@outlook.com>
2018-06-19 21:14:08 +0300

    Added gomeWorkLesson4

---
+++ ITVDN Csh starter/homeWorkLesson4/homeWorkLesson4.1/Program.cs
@@ -0,0 +1,36 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+/*  
+ * Создайте строковую переменную с именем inputString и в той же строке 
+ * проинициализируйте ее считанной из консоли строкой. Затем, добавьте 
+ * условный оператор, который должен выполнять следующее: если длина 
+ * введенной строки равна 5 символам, то вывести в консоль 
+ * "Да, длина строки равна 5", иначе – вывести "Нет, длина строки не равна 5". 
+ * Выводите данные на консоль с помощью метода WriteLine
+ */
+
+namespace homeWorkLesson4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string inputString = Console.ReadLine();
+
+            if (inputString.Length == 5) {
+                Console.WriteLine("Да, длина строки равна 5");
+            } else {
+                Console.WriteLine("Нет, длина строки не равна 5");
+            }
+
+        }
+    }
+}
