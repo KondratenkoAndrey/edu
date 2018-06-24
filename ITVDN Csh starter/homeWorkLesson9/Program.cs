f966b63af054cac431db3460ca34b3ee442240d5
Andrey <kondratenkoav@outlook.com>
2018-06-25 00:15:42 +0300

    Added homeWorkLesson9

---
+++ ITVDN Csh starter/homeWorkLesson9/Program.cs
@@ -0,0 +1,52 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+
+/*
+ * В класс MyClass поместите функцию int[] PairValuesArray(int n), 
+ * которая будет возвращать массив упорядоченных по возрастанию ЧЁТНЫХ 
+ * натуральных чисел в промежутке от 0 (включительно!) до n включительно.
+ * Разрешается использовать только классы непосредственно из пространства 
+ * имен System, все вложенные в него пространства имен использовать 
+ * запрещается.
+*/
+
+using System;
+
+namespace arr
+{
+    class MainClass
+    {
+
+        static int[] PairValuesArray(int n)
+        {
+
+            int[] array = new int[(n / 2) + 1];
+            int j = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                if ((i % 2) == 0) {
+                    array[j] = i;
+                    j++;
+                }
+            }
+
+            return array;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr = PairValuesArray(10);
+
+            for (int i = 0; i < arr.Length; i++) {
+                Console.Write("{0} ", arr[i]);
+            }
+
+        }
+    }
+}
---
+++ ITVDN Csh starter/homeWorkLesson9/sumOfArraysElements/Program.cs
@@ -0,0 +1,30 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace sumOfArraysElements
+{
+    class MainClass
+    {
+        static int SumArguments(params int[] array) {
+            
+            int result = 0;
+
+            for (int i = 0; i < array.Length; i++) {
+                result += array[i];
+            }
+
+            return result;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(SumArguments(0, 2, 5, 5, 1));
+        }
+    }
+}
