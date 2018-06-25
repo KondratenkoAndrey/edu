f8935599c7fe74e4b3eed2fb91e2c1c07329461b
Andrey <kondratenkoav@outlook.com>
2018-06-25 21:17:48 +0300

    Added homeWorkLesson2

---
+++ ITVDN Csh starter/homeWorkLesson2/homeWorkLesson2.1/Program.cs
@@ -0,0 +1,24 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace homeWorkLesson2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double pi = 3.141592653;
+            decimal e = 2.7182818284590452m;
+
+            Console.WriteLine("pi = {0}", pi);
+            Console.WriteLine("e = {0}", e);
+
+        }
+    }
+}
---
+++ ITVDN Csh starter/homeWorkLesson2/homeWorkLesson2.2/Program.cs
@@ -0,0 +1,29 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace homeWorkLesson2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string someVar1 = "some string 1";
+            string someVar2 = "\nsome string 2";
+            string someVar3 = "\tsome string 3";
+            string someVar4 = "\asome string 4";
+        
+            Console.WriteLine(someVar1);
+            Console.WriteLine(someVar2);
+            Console.WriteLine(someVar3);
+            Console.WriteLine(someVar4);
+
+        }
+    }
+}
