10ffb864dcd247f580ed3972a549551da06a779e
Andrey <kondratenkoav@outlook.com>
2018-06-26 21:49:47 +0300

    Changed homeWorkLesson5

---
+++ ITVDN Csh starter/homeWorkLesson5/homeWorkLesson5.1/Program.cs
@@ -0,0 +1,35 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace homeWorkLesson5
+{
+    
+    class MainClass
+
+    {
+        public static bool isEven(int number) {
+
+            //return (number % 2) == 0; // вариант 1
+
+            return (number & 0x01) == 0;
+                
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] numbers = {0, 1, 2, 3, 4, 5};
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine("{0} is {1}", item, isEven(item) ? "Even" : "Odd");
+            }
+
+        }
+    }
+}
