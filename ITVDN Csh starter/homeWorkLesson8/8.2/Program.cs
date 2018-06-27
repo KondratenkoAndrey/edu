57fafafa638a74fdeb56937dfdcd98ecd8eb3db5
Andrey <kondratenkoav@outlook.com>
2018-06-27 21:05:49 +0300

    Changed homeWorkLesson8

---
+++ ITVDN Csh starter/homeWorkLesson8/8.2/Program.cs
@@ -0,0 +1,40 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace Application
+{
+    class MainClass
+    {
+
+        static void PrintInfoAboutCreditAfterPayment(int payment) {
+            
+            const int amountOfCredit = 700;
+
+            int balance = amountOfCredit - payment;
+
+            Console.WriteLine("Информация о состоянии кредита:");
+
+            if (balance > 0) {
+                Console.WriteLine("Задолженность составляет {0}", balance);
+            } else if (balance < 0) {
+                Console.WriteLine("Переплата составляет {0}", balance * -1);
+            } else {
+                Console.WriteLine("Долг отсутствует");
+            }
+
+        }
+
+        public static void Main(string[] args)
+        {
+
+            PrintInfoAboutCreditAfterPayment(800);
+
+        }
+    }
+}
---
+++ ITVDN Csh starter/homeWorkLesson8/8.3/Program.cs
@@ -0,0 +1,32 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        static int CalculateRoutesCount(int n) {
+
+            if (n == 0) {
+                return 1;
+            } else {
+                return n * CalculateRoutesCount(n - 1);
+            }
+
+        }
+
+        public static void Main(string[] args)
+        {
+
+            int n = 5;
+            Console.WriteLine(CalculateRoutesCount(n));
+
+        }
+    }
+}
