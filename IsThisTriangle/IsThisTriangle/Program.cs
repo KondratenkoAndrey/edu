c1558a7933f19a2a54ae89644cc6142741975e66
Andrey <kondratenkoav@outlook.com>
2018-06-29 00:00:35 +0300

    Is This Triangle?

---
+++ IsThisTriangle/IsThisTriangle/Program.cs
@@ -0,0 +1,28 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace IsThisTriangle
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0, b = 0, c = 0;
+            Random rnd = new Random();
+
+            for (int i = 0; i < 100; i++) {
+                a = rnd.Next(1, 100);
+                b = rnd.Next(1, 100);
+                c = rnd.Next(1, 100);
+                Console.WriteLine(Triangle.IsTriangle(a, b, c));
+            }
+
+        }
+    }
+}
---
+++ IsThisTriangle/IsThisTriangle/Triangle.cs
@@ -0,0 +1,22 @@
+﻿//
+// Triangle.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+namespace IsThisTriangle
+{
+    public class Triangle
+    {
+
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            //треугольник возможно построить,
+            //если сумма каждой пары сторон больше третьей стороны
+            return (a + b > c && a + c > b && b + c > a);
+        }
+
+    }
+}
