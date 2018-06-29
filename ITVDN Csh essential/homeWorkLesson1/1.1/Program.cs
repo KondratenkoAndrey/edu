62537346ee4b566a84bdfad9ecc1cc3cb0e1b12f
Andrey <kondratenkoav@outlook.com>
2018-06-29 20:33:40 +0300

    Added homework Lesson 1.1 Classes

---
+++ ITVDN Csh essential/homeWorkLesson1/1.1/Program.cs
@@ -0,0 +1,27 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Address someAddress = new Address(248000,
+                                             "Rassia",
+                                             "Kaluga",
+                                             "Main",
+                                             23,
+                                              154);
+            someAddress.PrintAddress();            
+        
+        }
+    }
+}
