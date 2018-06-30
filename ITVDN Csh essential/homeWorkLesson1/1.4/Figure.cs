726a7ac067cf9fa224f794b41ff643649d745f31
Andrey <kondratenkoav@outlook.com>
2018-06-30 23:58:13 +0300

    Added homework Lesson 1.4 Classes

---
+++ ITVDN Csh essential/homeWorkLesson1/1.4/Figure.cs
@@ -0,0 +1,76 @@
+﻿//
+// Figure.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+namespace Application
+{
+    public class Figure
+    {
+        public Figure(Point point1,
+                      Point point2,
+                      Point point3)
+        {
+            points = new Point[]
+            {
+                point1,
+                point2,
+                point3
+            };
+        }
+
+        public Figure(Point point1,
+                      Point point2,
+                      Point point3,
+                      Point point4)
+        {
+            points = new Point[]
+            {
+                point1,
+                point2,
+                point3,
+                point4            
+            };
+        }
+
+        public Figure(Point point1,
+                      Point point2,
+                      Point point3,
+                      Point point4,
+                      Point point5)
+        {
+            points = new Point[]
+            {
+                point1,
+                point2,
+                point3,
+                point4,
+                point5
+            };
+        }
+
+        private Point[] points;
+
+        private double LengthSide(Point a, Point b)
+        {
+            var test = Math.Sqrt((Math.Pow((b.X - a.X), 2) + Math.Pow((b.Y - a.Y), 2)));
+            return test;
+        }
+
+        public void printPerimeter()
+        {
+            double perimeter = 0;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                perimeter += LengthSide(points[i], points[(i + 1) % points.Length]);
+            }
+
+            Console.WriteLine("Polygon has perimeter = {0}", perimeter);
+        }
+
+    }
+}
---
+++ ITVDN Csh essential/homeWorkLesson1/1.4/Program.cs
@@ -0,0 +1,29 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Point point1 = new Point(0, 0);
+            Point point2 = new Point(0, 10);
+            Point point3 = new Point(20, 10);
+            Point point4 = new Point(20, 0);
+
+            Figure someFigure = new Figure(point1,
+                                           point2,
+                                           point3,
+                                           point4);
+            someFigure.printPerimeter();
+
+        }
+    }
+}
