b3226c76248ff834ece50b9bb913f3db2f7ab41a
Andrey <kondratenkoav@outlook.com>
2018-07-01 21:54:19 +0300

    Remove The Minimum

---
+++ RemoveTheMinimum/RemoveTheMinimum/Program.cs
@@ -0,0 +1,22 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+using System.Collections.Generic;
+
+namespace RemoveTheMinimum
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int> {};
+            List<int> changedList = Remover.RemoveSmallest(list);
+
+        }
+    }
+}
---
+++ RemoveTheMinimum/RemoveTheMinimum/Remover.cs
@@ -0,0 +1,42 @@
+﻿//
+// Remover.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+using System.Collections.Generic;
+namespace RemoveTheMinimum
+{
+    public class Remover
+    {
+        public static List<int> RemoveSmallest(List<int> numbers)
+        {
+            //если пустой массив, то и возвращаяем пустой
+            if (numbers.Count == 0)
+            {
+                return numbers;
+            }
+
+            //создаем новый.ю чтобы не изменить исходный
+            List<int> result = new List<int>(numbers);
+
+            //ищем минимальное значение
+            int minValue = result[0];
+
+            foreach (var item in result)
+            {
+                if (item < minValue)
+                {
+                    minValue = item;
+                }
+            }
+
+             //удалаем первое минимальное значение
+            result.RemoveAt(result.IndexOf(minValue));
+
+            return result;
+        }
+    }
+}
