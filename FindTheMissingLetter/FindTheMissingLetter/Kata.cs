51e7071a8e43f3f0bf1644143b59c8747bf4b1aa
Andrey <kondratenkoav@outlook.com>
2018-07-05 22:22:56 +0300

    Find The Missing Letter

---
+++ FindTheMissingLetter/FindTheMissingLetter/Kata.cs
@@ -0,0 +1,25 @@
+﻿//
+// Kata.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+namespace FindTheMissingLetter
+{
+    public class Kata
+    {
+        public static char FindMissingLetter(char[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if ((int)array[i] != (int)array[i + 1] - 1)
+                {
+                    return (char)((int)array[i] + 1);
+                }
+            }
+            return '\0';
+        }
+    }
+}
