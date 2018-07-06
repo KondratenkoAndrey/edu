917a88639ea36b9467f49bf8131ca5952496eb6e
Andrey <kondratenkoav@outlook.com>
2018-07-06 19:29:10 +0300

    Added home work lesson 2.2

---
+++ ITVDN Csh essential/homeWorkLesson2/2.2/Converter.cs
@@ -0,0 +1,34 @@
+﻿//
+// Converter.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+namespace Application
+{
+    public class Converter
+    {
+        public Converter(double usd, double eur)
+        {
+            this.usd = usd;
+            this.eur = eur;
+        }
+
+        double usd;
+        double eur;
+
+        public double RubToUsd(double rub)
+        {
+            return rub / usd;
+        }
+
+        public double RubToEur(double rub)
+        {
+            return rub / eur;
+        }
+
+    }
+
+}
---
+++ ITVDN Csh essential/homeWorkLesson2/2.2/Program.cs
@@ -0,0 +1,23 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Converter converter = new Converter(63.25D, 74.38D);
+
+            Console.WriteLine("{0:f2}", converter.RubToUsd(1000));
+            Console.WriteLine("{0:f2}", converter.RubToEur(1000));
+        }
+    }
+}
