e1bfcdf335b85ffbc54284f627ac8183bd6a54ac
Andrey <kondratenkoav@outlook.com>
2018-07-06 08:09:43 +0300

    Find The Odd Int

---
+++ FindTheOddInt/FindTheOddInt/Kata.cs
@@ -0,0 +1,40 @@
+﻿//
+// Kata.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+using System.Collections.Generic;
+namespace FindTheOddInt
+{
+    public class Kata
+    {
+        public static int find_it(int[] seq)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            foreach (var number in seq)
+            {
+                if (!dict.ContainsKey(number))
+                {
+                    dict.Add(number, 1);
+                }
+                else
+                {
+                    dict[number]++;
+                }
+            }
+
+            foreach (var number in dict)
+            {
+                if ((number.Value % 2) == 1)
+                {
+                    return number.Key;
+                }
+            }
+            return -1;
+        }
+    }
+}
