4dee343942f7a21d4c4f596dd2169a90caecf94e
Andrey <kondratenkoav@outlook.com>
2018-07-06 18:50:21 +0300

    Added home work lesson 2.1

---
+++ ITVDN Csh essential/homeWorkLesson2/2.1/Program.cs
@@ -0,0 +1,24 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            User someUser = new User("06.07.2018",
+                                    "User",
+                                    "Andrey",
+                                    "Petrov",
+                                    19);
+            someUser.PrintInfo();
+        }
+    }
+}
