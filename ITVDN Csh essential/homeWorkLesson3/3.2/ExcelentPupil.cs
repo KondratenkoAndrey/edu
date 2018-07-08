c28ef9316ddeb7d426f34de797837f7a7128d6ce
Andrey <kondratenkoav@outlook.com>
2018-07-08 23:11:21 +0300

    Added home work lesson 3.2

---
+++ ITVDN Csh essential/homeWorkLesson3/3.2/ExcelentPupil.cs
@@ -0,0 +1,38 @@
+﻿//
+// ExcelentPupil.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+namespace Application
+{
+    public class ExcelentPupil : Pupil
+    {
+        public ExcelentPupil()
+        {
+        }
+
+        public override void Study()
+        {
+            Console.WriteLine("The pupil are excelent studying");
+        }
+
+        public override void Read()
+        {
+            Console.WriteLine("The pupil are excelent reading");
+        }
+
+        public override void Write()
+        {
+            Console.WriteLine("The pupil are excelent writing");
+        }
+
+        public override void Relax()
+        {
+            Console.WriteLine("The pupil are excelent relaxing");
+        }
+
+    }
+}
---
+++ ITVDN Csh essential/homeWorkLesson3/3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Pupil[] arr = { 
+                new BadPupil(), 
+                new GoodPupil(), 
+                new ExcelentPupil() };
+            ClassRoom classRoom = new ClassRoom(arr);
+            classRoom.PrintInfoAboutClass();
+        }
+       
+    }
+}
