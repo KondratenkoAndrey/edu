8c1ecd28c7b9d330373d9062a75916df2fe8eced
Andrey <kondratenkoav@outlook.com>
2018-07-12 13:20:46 +0300

    Added First non-repeating character

---
+++ FirstNonRepeatingCharacter/FirstNonRepeatingCharacter/KataTests.cs
@@ -0,0 +1,27 @@
+﻿//
+// KataTests.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using NUnit.Framework;
+using System;
+namespace FirstNonRepeatingCharacter
+{
+    [TestFixture()]
+    public class KataTests
+    {
+        [Test()]
+        public void BasicTests()
+        {
+
+            Assert.AreEqual("a", Kata.FirstNonRepeatingLetter("a"));
+            Assert.AreEqual("t", Kata.FirstNonRepeatingLetter("stress"));
+            Assert.AreEqual("e", Kata.FirstNonRepeatingLetter("moonmen"));
+            Assert.AreEqual("T", Kata.FirstNonRepeatingLetter("sTreSS"));
+            Assert.AreEqual("b", Kata.FirstNonRepeatingLetter("aAAb"));
+
+        }
+    }
+}
---
+++ FirstNonRepeatingCharacter/FirstNonRepeatingCharacter/Program.cs
@@ -0,0 +1,20 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace FirstNonRepeatingCharacter
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Let's do it!");
+
+        }
+    }
+}
