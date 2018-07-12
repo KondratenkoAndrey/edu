e85518a2320d4a8c9faa3848755ad3cd669d81c7
Andrey <kondratenkoav@outlook.com>
2018-07-12 23:39:35 +0300

    Added home work lesson 5.2 Indexers

---
+++ ITVDN Csh essential/homeWorkLesson5/5.3.Indexers/Article.cs
@@ -0,0 +1,39 @@
+﻿//
+// Article.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+namespace Indexers
+{
+    public class Article
+    {
+        public Article(string name, 
+                       string storeName, 
+                       double price)
+        {
+            this.name = name;
+            this.storeName = storeName;
+            this.price = price;
+        }
+
+        private string name;
+        private string storeName;
+        private double price;
+
+        public string Name => name;
+
+        public string GetInfo()
+        {
+            return string.Format("Article \"{0}\" " +
+                                 "present in \"{1}\" " +
+                                 "with price \"{2}\"",
+                                this.name,
+                                this.storeName,
+                                this.price);
+        }
+
+    }
+}
---
+++ ITVDN Csh essential/homeWorkLesson5/5.3.Indexers/Program.cs
@@ -0,0 +1,39 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace Indexers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Let's go!");
+
+            Store someStore = new Store(
+                new Article[]
+                {
+                    new Article("ball", "sport store", 12.45),
+                    new Article("bread", "food store", 2.12),
+                    new Article("milk", "milk store", 4.44)
+                }
+            );
+
+            someStore.PrintInfoAboutArticle(someStore[0]);
+            someStore.PrintInfoAboutArticle(someStore[1]);
+            someStore.PrintInfoAboutArticle(someStore[2]);
+            someStore.PrintInfoAboutArticle(someStore[3]);
+
+            someStore.PrintInfoAboutArticle(someStore["ball"]);
+            someStore.PrintInfoAboutArticle(someStore["bread"]);
+            someStore.PrintInfoAboutArticle(someStore["milk"]);
+            someStore.PrintInfoAboutArticle(someStore["water"]);
+
+        }
+    }
+}
