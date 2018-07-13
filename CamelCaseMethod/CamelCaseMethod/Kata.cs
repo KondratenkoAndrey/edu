226d1b54169228f68224facacd70af04662295f8
Andrey <kondratenkoav@outlook.com>
2018-07-13 13:26:55 +0300

    CamelCase method

---
+++ CamelCaseMethod/CamelCaseMethod/Kata.cs
@@ -0,0 +1,42 @@
+﻿//
+// Kata.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+namespace CamelCaseMethod
+{
+    public static class Kata
+    {
+
+        public static string CamelCase(this string str)
+        {
+            //если строка пустая, то возвращаем пустую
+            if (str == "") 
+            {
+                return "";
+            }
+
+            //делим строку на массив слов
+            string[] words = str.Split(' ');
+            string result = "";
+
+            //каждое слово берем первую букву и преобразуем ее в верхний 
+            //регистр, остальные в нижний
+            foreach (var word in words)
+            {
+                if (word != "")
+                {
+                    result += word.Substring(0, 1).ToUpper() +
+                              word.Substring(1).ToLower();
+                }
+
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ CamelCaseMethod/CamelCaseMethod/KataTests.cs
@@ -0,0 +1,32 @@
+﻿//
+// KataTests.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using NUnit.Framework;
+using System;
+namespace CamelCaseMethod
+{
+    [TestFixture()]
+    public class KataTests
+    {
+        [Test()]
+        public void BasicTests()
+        {
+
+            Assert.AreEqual("TestCase", "test case".CamelCase());
+            Assert.AreEqual("CamelCaseWord", "camel case word".CamelCase());
+            Assert.AreEqual("CamelCaseWord", "camel cASe worD".CamelCase());
+            Assert.AreEqual("Camel", "caMEl".CamelCase());
+            Assert.AreEqual("C", "c".CamelCase());
+            Assert.AreEqual("", "".CamelCase());
+            Assert.AreEqual("", " ".CamelCase());
+            Assert.AreEqual("", "   ".CamelCase());
+            Assert.AreEqual("AB", "a   b".CamelCase());
+
+
+        }
+    }
+}
