9e1883ca6c46224cf1756411c3bb91f8eae83e14
Andrey <kondratenkoav@outlook.com>
2018-07-17 14:12:42 +0300

    Added home work lesson 7 Structures

---
+++ ITVDN Csh essential/homeWorkLesson7/Structures_1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Structures_1
+{
+
+    struct Notebook
+    {
+
+        public Notebook(string model, string brand, double price)
+        {
+            this.model = model;
+            this.brand = brand;
+            this.price = price;
+        }
+
+        private string model;
+        private string brand;
+        private double price;
+
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+        }
+
+        public string Brand
+        {
+            get
+            {
+                return brand;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+        }
+
+        public void PrintInfo()
+        {
+            string info = string.Format("Notebook {0} {1}, price: {2:f2}",
+                brand,
+                model,
+                price);
+            Console.WriteLine(info);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Notebook someNote;
+            //someNote.PrintInfo();
+
+            Notebook someNote = new Notebook();
+            someNote.PrintInfo();
+
+            Notebook anotherNotebook = new Notebook("macbook air", "Apple", 999.99);
+            anotherNotebook.PrintInfo();
+
+        }
+    }
+}
