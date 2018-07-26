ed52864f139ab50eca4bdbca96ca24707e4a000d
Andrey <kondratenkoav@outlook.com>
2018-07-26 21:22:18 +0300

    Human Readable Time

---
+++ HumanReadableTime/HumanReadableTime/TimeFormat.cs
@@ -0,0 +1,23 @@
+﻿//
+// TimeFormat.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+namespace HumanReadableTime
+{
+    public static class TimeFormat
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            int h = seconds / 3600;
+            int m = seconds / 60 % 60;
+            int s = seconds % 60;
+            return h.ToString().PadLeft(2, '0') + ":" +
+                    m.ToString().PadLeft(2, '0') + ":" +
+                    s.ToString().PadLeft(2, '0');
+        }
+    }
+}
