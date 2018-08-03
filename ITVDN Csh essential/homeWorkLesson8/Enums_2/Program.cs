87488eb9a07fac0d4ff3f2a586de9773f43eee96
Andrey <kondratenkoav@outlook.com>
2018-08-03 20:35:24 +0300

    Home work 8. Enums_1

---
+++ ITVDN Csh essential/homeWorkLesson8/Enums_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace Enums_2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
