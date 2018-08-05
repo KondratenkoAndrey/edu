3ad33bfd06795b068d1d3f7447420aa8a3ca5485
Andrey <kondratenkoav@outlook.com>
2018-08-05 21:43:16 +0300

    Added delegates from Metanit

---
+++ Metanit/40_Delegates/Delegates_1/Program.cs
@@ -0,0 +1,47 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+
+namespace _Delegates_1
+{
+    class MainClass
+    {
+        delegate void Message(string str = "Default string");
+        delegate int Operation(int x, int y); 
+
+        public static void Main(string[] args)
+        {
+            Message message;
+            //message = new Message(Print);
+            message = Print;
+
+            //message.Invoke();
+            message();
+
+            Operation operation = Add;
+            int result = operation(4, 5);
+            message($"Result of operation is {result}");
+
+        }
+
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        static void Print(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
