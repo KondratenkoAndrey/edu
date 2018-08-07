108c09d51f73596a84dd2455530b54480c3d4e4b
Andrey <kondratenkoav@outlook.com>
2018-08-07 22:00:54 +0300

    Round by 0.5 steps

---
+++ RoundBy0.5Steps/RoundBy0.5Steps/Kata.cs
@@ -0,0 +1,19 @@
+﻿//
+// Kata.cs
+//
+// Author:
+//       Andrey Kondratenko <>
+//
+// Copyright (c) 2018 Andrey Kondratenko
+using System;
+namespace RoundBy0.Steps
+{
+    public class Kata
+    {
+        public static double Solution(double n)
+        {
+            //without Math is more interesting))
+            return (int)n + ((int)((n * 10 % 10 / 10) / 0.25) + 1) / 2 / 2.0;
+        }
+    }
+}
