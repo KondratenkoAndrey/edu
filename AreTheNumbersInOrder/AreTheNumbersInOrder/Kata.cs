27685756fe935b85d67ebc844a42364ab05f8147
Andrey Kondratenko <kondratenkoav@outlook.com>
2018-09-03 21:39:16 +0300

    Are the numbers in order?

---
+++ AreTheNumbersInOrder/AreTheNumbersInOrder/Kata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace AreTheNumbersInOrder
+{
+    class Kata
+    {
+        public static bool IsAscOrder(int[] arr)
+        {
+            //without linq
+            /*
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i + 1]) return false;
+            }
+            return true;
+            */
+
+            //with linq
+            return arr.OrderBy(x => x).SequenceEqual(arr);
+        }
+    }
+}
---
+++ AreTheNumbersInOrder/AreTheNumbersInOrder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreTheNumbersInOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.IsAscOrder(new int[] { 1, 1, 2, 4, 7, 19 }));// == true
+            Console.WriteLine(Kata.IsAscOrder(new int[] { 1, 2, 3, 4, 5 }));// == true
+            Console.WriteLine(Kata.IsAscOrder(new int[] { 1, 6, 10, 18, 2, 4, 20 }));// == false
+            Console.WriteLine(Kata.IsAscOrder(new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 }));// == false
+        }
+    }
+}
