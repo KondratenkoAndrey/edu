d3ec0e522b0112a9e21466d3880dccce06464e6b
Andrey Kondratenko <kondratenkoav@outlook.com>
2018-09-10 20:21:48 +0300

    Convert string to camel case

---
+++ ConvertStringToCamelCase/ConvertStringToCamelCase/Kata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConvertStringToCamelCase
+{
+    class Kata
+    {
+        public static string ToCamelCase(string str)
+        {
+            return Regex.Replace(str, 
+                                @"[-_]\w", 
+                                m => m.ToString().Remove(0,1).ToUpper());
+        }
+    }
+}
