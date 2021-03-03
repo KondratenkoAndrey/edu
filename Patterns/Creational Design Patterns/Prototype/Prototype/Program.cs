dbb223c74d60e0a525eedb9313d15c8dd2b678e5
Андрей Кондратенко <264794@gmail.com>
2021-03-03 15:34:34 +0300

    Паттерны. Компоновщик

---
+++ Patterns/Creational Design Patterns/Prototype/Prototype/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Prototype
+{
+    public class Person : ICloneable
+    {
+        public int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public IdInfo IdInfo;
+
+        public Person ShallowCopy()
+        {
+            return (Person) this.MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        {
+            Person clone = (Person) this.MemberwiseClone();
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            clone.Name = string.Copy(Name);
+            return clone;
+        }
+
+        public object Clone()
+        {
+            var clone = (Person) this.MemberwiseClone();
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            return clone;
+        }
+    }
+
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var p1 = new Person
+            {
+                Age = 42,
+                BirthDate = Convert.ToDateTime("1977-01-01"),
+                Name = "Jack Daniels",
+                IdInfo = new IdInfo(666)
+            };
+
+            // Выполнить поверхностное копирование p1 и присвоить её p2.
+            var p2 = p1.ShallowCopy();
+
+            // Сделать глубокую копию p1 и присвоить её p3.
+            var p3 = p1.DeepCopy();
+
+            // Сделать копию через метод Clone интерфейса IClonable
+            var p4 = (Person) p1.Clone();
+
+            // Вывести значения p1, p2 и p3 и p4.
+            Console.WriteLine("Original values of p1, p2, p3:");
+            Console.WriteLine("   p1 instance values: ");
+            DisplayValues(p1);
+            Console.WriteLine("   p2 instance values:");
+            DisplayValues(p2);
+            Console.WriteLine("   p3 instance values:");
+            DisplayValues(p3);
+            Console.WriteLine("   p4 instance values:");
+            DisplayValues(p4);
+
+            // Изменить значение свойств p1 и отобразить значения p1, p2 и p3 и p4.
+            p1.Age = 32;
+            p1.BirthDate = Convert.ToDateTime("1900-01-01");
+            p1.Name = "Frank";
+            p1.IdInfo.IdNumber = 7878;
+            Console.WriteLine("\nValues of p1, p2 and p3 after changes to p1:");
+            Console.WriteLine("   p1 instance values: ");
+            DisplayValues(p1);
+            Console.WriteLine("   p2 instance values (reference values have changed):");
+            DisplayValues(p2);
+            Console.WriteLine("   p3 instance values (everything was kept the same):");
+            DisplayValues(p3);
+            Console.WriteLine("   p4 instance values (everything was kept the same):");
+            DisplayValues(p4);
+        }
+
+        private static void DisplayValues(Person p)
+        {
+            Console.WriteLine("      Name: {0:s}, Age: {1:d}, BirthDate: {2:MM/dd/yy}",
+                p.Name, p.Age, p.BirthDate);
+            Console.WriteLine("      ID#: {0:d}", p.IdInfo.IdNumber);
+        }
+    }
+}
